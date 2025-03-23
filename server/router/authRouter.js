a98fc66bc927339335b70614a324a01761ce2afa
Vicky <vickygupta9987@gmail.com>
2025-03-24 01:02:27 +0530

    commit

--- server/router/authRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginController, registerController } from "../controller/authController.js";
-
-const authRouter = express.Router();
-
-authRouter.post("/login", loginController);
-authRouter.post("/register", registerController);
-
-export default authRouter;
--- server/router/chatRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllUserController, getMessages } from "../controller/chatController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
-const chatRouter = express.Router();
-
-chatRouter.get('/users',authMiddleware, getAllUserController);
-chatRouter.get('/messages/:userId/:recipientId',authMiddleware, getMessages);
-
-export default  chatRouter;
